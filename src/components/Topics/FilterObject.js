e890b9062b9bee43371af7f39a5848f120fd73ce
CameronK24 <cskelly15@gmail.com>
2020-06-17 16:01:19 -0600

    working through filterObject

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,63 @@
 import React, {Component} from 'react';
 
 class FilterObject extends Component {
+    constructor() {
+        super();
+        this.state = {
+            unFilteredArray: [{
+                make: 'Ford',
+                model: 'Mustang',
+                year: 2016,
+                damage: true,
+                spoiler: false
+            }, {
+                make: 'Nissan',
+                model: 'Maxima',
+                year: 2020,
+                color: 'Lightning Blue',
+                spoiler: true
+            }, {
+                make: 'Aston Martin',
+                model: 'DB9',
+                year: 2016,
+                color: 'Gray',
+                damage: true
+            }],
+            userInput: '',
+            filteredArray: []
+        }
+    }
+
+    handleUserInput = (input) => {
+        // console.log(input);
+        this.setState({
+            userInput: input
+        })
+    }
+
+    solveProblem = (input) => {
+        let filter = [];
+        for(let x = 0; x < this.state.unFilteredArray.length; x++) {
+            for(let key in this.state.unFilteredArray[x]) {
+                if (key === input) {
+                    filter.push(this.state.unFilteredArray[x]);
+                }
+            }
+        }
+        console.log(filter);
+        this.state.filteredArray = filter;
+        
+    }
+
     render() {
         return (
-            <p>FilterObject Component</p>
+            <div className="puzzleBox filterObjectPB">
+                <h4>Filter Object</h4>
+                <span className="puzzleText">Original: {JSON.stringify(this.state.unFilteredArray, null, 10)}</span>
+                <input className="inputLine" onChange={(event) => this.handleUserInput(event.target.value)}/>
+                <button className="confirmationButton" onClick={this.solveProblem(this.state.userInput)}>Filter</button>
+                <span className="resultsBox filterObjectRB">Filtered: {JSON.stringify(this.state.filteredArray, null, 10)}</span>
+            </div>
         )
     }
 }
